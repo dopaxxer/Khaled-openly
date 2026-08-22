535cc475cae9e6e989d2fca4adddaa89dca7c35a
dopaxxer <ccc6c9@gmail.com>
2026-08-22 22:28:41 +0200

    Add mobile Link shim

---
+++ mobile-spa/src/next-link.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { navigate } from './next-navigation'
+
+export default function Link({ href = '/', onClick, children, target, ...props }) {
+  const url = typeof href === 'string' ? href : href?.pathname || '/'
+  const external = /^https?:\/\//i.test(url) || url.startsWith('mailto:') || url.startsWith('tel:')
+
+  return <a href={url} target={target} {...props} onClick={event => {
+    onClick?.(event)
+    if (event.defaultPrevented || external || target === '_blank' || event.metaKey || event.ctrlKey || event.shiftKey || event.altKey) return
+    event.preventDefault()
+    navigate(url)
+  }}>{children}</a>
+}
