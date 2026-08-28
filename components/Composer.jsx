6bd309a4fc14a24e4b6b2ca6bdd29fd8c70464dd
dopaxxer <ccc6c9@gmail.com>
2026-08-28 20:12:56 +0200

    Keep composer media actions aligned with implemented attachments

--- components/Composer.jsx
+++ components/Composer.jsx
@@ -173,7 +173,7 @@ export function Composer({ firstPost = false, inline = false, onPublished = null
   if (inline && !open) {
     return <button type="button" className="composer-prompt panel v2-composer-prompt" onClick={() => setOpen(true)}>
       <span className="v2-composer-prompt-copy">ماذا تريد أن تقول؟</span>
-      <span className="v2-composer-prompt-tools" aria-hidden="true">+ music&nbsp;&nbsp;&nbsp;+ book&nbsp;&nbsp;&nbsp;+ film</span>
+      <span className="v2-composer-prompt-tools" aria-hidden="true">+ music</span>
       <span className="v2-composer-prompt-post">Post</span>
     </button>
   }
